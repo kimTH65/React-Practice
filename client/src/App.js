6fe35f13295386fb4756901594efbdbe9ff29cd7
AIS-NB-102\NB-102 <xogns6515@gmail.com>
2022-06-08 09:38:13 +0900

    test

--- client/src/App.js
+++ client/src/App.js
@@ -8,55 +8,11 @@ import TableBody from '@mui/material/TableBody';
 import Item from './components/Item'
 import List from './components/List'
 class App extends React.Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          items:''
-      }
-  }
-
-  setRefresh = () => {
-      this.setState({items:''});
-      this.callApi().then(res => this.setState({items: res}))
-                      .catch(err => console.log(err));
-  }
-
-  componentDidMount() {
-      this.callApi().then(res => this.setState({items: res}))
-                      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-      const response = await fetch('/load');
-      const body = await response.json();
-      return body;
-  }
+  
 
   render(){
     return(
-      
-      <Table>
-        <TableHead>
-            <TableRow>
-                <TableCell>id</TableCell>
-                <TableCell>title</TableCell>
-                <TableCell>check</TableCell>
-                <TableCell>onRemove</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            {this.state.items ? this.state.items.map(c =>{
-                return(
-                    <Item
-                        id={c.id}
-                        title={c.title}
-                        toggle={c.onToggle}
-                        remove={c.onRemove}
-                    />
-                )
-            }):""}
-        </TableBody>
-      </Table>
+      <List/>
     )
   }
 }
