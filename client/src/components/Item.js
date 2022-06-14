bcb407ad598ee12100e7d83db8c6ca06ae6d28a0
AIS-NB-102\NB-102 <xogns6515@gmail.com>
2022-06-14 09:28:24 +0900

    soft delete 구현

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -6,13 +6,13 @@ import Delete from './Delete';
 class Item extends React.Component{
     
     render() {
-        const { id, title, onToggle, onRemove} = this.props
+        const { id, title, onToggle,toggle, remove} = this.props
         return(
             <TableRow className='item' onClick={() => onToggle(id)}>
                 <TableCell>{id}</TableCell>
                 <TableCell>{title}</TableCell>
-                <TableCell>{onToggle === 'check' && '✓'}</TableCell>
-                <TableCell>{onRemove === "removed" ? 'Ⅹ' : <Delete id={this.props.id}/>}</TableCell>
+                <TableCell>{toggle === 'check' && '✓'}</TableCell>
+                <TableCell>{remove === "removed" ? 'Ⅹ' : <Delete id={this.props.id}/>}</TableCell>
             </TableRow>
         );
 
